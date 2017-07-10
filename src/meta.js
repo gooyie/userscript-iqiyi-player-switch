6889f3e4a32f772e6982435fb199890f0d364cc2
gooyie <zhengzh1003@gmail.com>
2017-07-10 13:31:14 +0800

    支持外链 *://www.zybus.com/*

--- src/meta.js
+++ src/meta.js
@@ -4,7 +4,7 @@
 // @homepageURL  https://github.com/gooyie/userscript-iqiyi-player-switch
 // @supportURL   https://github.com/gooyie/userscript-iqiyi-player-switch/issues
 // @updateURL    https://raw.githubusercontent.com/gooyie/userscript-iqiyi-player-switch/master/dist/iqiyi-player-switch.user.js
-// @version      1.10.1
+// @version      1.10.2
 // @description  爱奇艺flash播放器与html5播放器随意切换，改善html5播放器播放体验。
 // @author       gooyie
 // @license      MIT License
@@ -12,6 +12,7 @@
 // @include      *://*.iqiyi.com/*
 // @include      *://v.baidu.com/*
 // @include      *://music.baidu.com/mv/*
+// @include      *://www.zybus.com/*
 // @grant        GM_registerMenuCommand
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
