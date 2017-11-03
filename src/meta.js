1592ae61d27bd2258395253881ef997b910ba953
gooyie <zhengzh1003@gmail.com>
2017-11-03 13:32:08 +0800

    使用 babel-preset-env 代替 babel-preset-es2015 并更新 devDependencies

--- src/meta.js
+++ src/meta.js
@@ -7,8 +7,11 @@ module.exports = `
 // @homepageURL  ${pkg.homepage}
 // @supportURL   ${pkg.bugs.url}
 // @updateURL    https://raw.githubusercontent.com/${pkg.author}/userscript-iqiyi-player-switch/master/dist/${pkg.name}.user.js
-// @version      ${pkg.version}
 // @description  ${pkg.description}
+// @version      ${pkg.version}
+// @compatible   chrome >= 43
+// @compatible   firefox >= 45
+// @compatible   edge >= 15
 // @author       ${pkg.author}
 // @license      ${pkg.license} License
 //
